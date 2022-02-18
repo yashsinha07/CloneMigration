bd8f9a4a66778ca59ad549496fcd6e978d9cd023
Yash Sinha <yash.sinha@inmobi.com>
2022-02-18 14:54:49 +0530

    Response code change added.

--- src/main/java/com/yashsinha/clonemigration/controllers/cloneMigrationController.java
+++ src/main/java/com/yashsinha/clonemigration/controllers/cloneMigrationController.java
@@ -3,6 +3,7 @@ package com.yashsinha.clonemigration.controllers;
 import com.yashsinha.clonemigration.model.DataJSON;
 import com.yashsinha.clonemigration.model.ResponseClass;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,14 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class cloneMigrationController {
-    
-    @GetMapping("/hello")
-    public String test(){
-        return "hello";
-    }
 
     @GetMapping(value="/app/{orgId}/migrate")
-    public ResponseClass migrateOrganisationClients(@RequestHeader("Authorization") String header, @PathVariable String orgId){
+    public ResponseEntity<ResponseClass> migrateOrganisationClients(@RequestHeader("Authorization") String header, @PathVariable String orgId){
 
         int id = Integer.parseInt(orgId);
 
@@ -29,12 +25,9 @@ public class cloneMigrationController {
             dataJSON.setMessage("Migration Successful.");
             dataJSON.setSuccess(true);
             response.setData(dataJSON);
-        } else {
-            response.setHttpCode(HttpStatus.BAD_REQUEST.toString());
-            dataJSON.setMessage("Migration failed.");
-            dataJSON.setSuccess(false);
-            response.setData(dataJSON);
+        } else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
