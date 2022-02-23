dc3501c8b190093f7ec57af15662642071e2c79d
Yash Sinha <yash.sinha@inmobi.com>
2022-02-23 17:13:59 +0530

    Request Param added.

--- src/main/java/com/yashsinha/clonemigration/controllers/cloneMigrationController.java
+++ src/main/java/com/yashsinha/clonemigration/controllers/cloneMigrationController.java
@@ -4,16 +4,15 @@ import com.yashsinha.clonemigration.model.DataJSON;
 import com.yashsinha.clonemigration.model.ResponseClass;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class cloneMigrationController {
 
     @GetMapping(value="/app/{orgId}/migrate")
-    public ResponseEntity<ResponseClass> migrateOrganisationClients(@RequestHeader("Authorization") String header, @PathVariable String orgId){
+    public ResponseEntity<ResponseClass> migrateOrganisationClients(@RequestHeader("Authorization") String header,
+                                                                    @PathVariable String orgId,
+                                                                    @RequestParam String redirectUrl){
 
         int id = Integer.parseInt(orgId);
 
